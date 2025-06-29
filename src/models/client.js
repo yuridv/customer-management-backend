e92cd03fc2db16f0edae99d440dbaea331105451
Yuri Eduardo <57859317+yuridv@users.noreply.github.com>
2025-06-29 00:35:11 -0300

    fix error in model clients

--- src/models/client.js
+++ src/models/client.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 
 const schema = new mongoose.Schema({
   name: { type: String, required: true },
-  phone: { type: Number, required: true, unique: true },
-  cpf: { type: Number, required: true, unique: true },
+  phone: { type: String, required: true, unique: true },
+  cpf: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   birthday: { type: Date, required: true },
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
